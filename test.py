2cee901a63260abe8f0fbe81dd8bb4501480eb6d
siphera <salmansiphenkosi@gmail.com>
2020-11-18 18:19:36 +0200

    task instructions

--- test.py
+++
@@ -1,7 +0,0 @@
-class Tickets:
-    def __init__(self, cellnumber, category, nr_tickets):
-        self.cellnumber = cellnumber
-        self.category = category
-        self.nr_tickets = nr_tickets
-
-
